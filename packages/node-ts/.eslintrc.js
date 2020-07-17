e8a50e49fb878f5b6e97baa5aaa9fb712d10e516
Travis Reynolds <travis@cirruscreative.co.uk>
2020-07-17 12:29:13 +0000

    Add node-ts package

---
+++ packages/node-ts/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    indent: ['error', 2],
+    semi: 'off',
+    camelcase: 'off',
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    camelcase: ['off', {
+      properties: 'never',
+      ignoreDestructuring: true
+    }],
+    'comma-dangle': ['error', 'never'],
+    'arrow-parens': ['error', 'as-needed'],
+    'linebreak-style': 0,
+    'object-curly-spacing': ['error', 'always'],
+    'computed-property-spacing': ['error', 'always'],
+    'space-in-parens': ['error', 'never'],
+    'array-bracket-spacing': ['error', 'never'],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all'
+      }
+    ],
+    '@typescript-eslint/semi': ['error', 'never']
+    'prettier/prettier': 'error'
+  }
+}

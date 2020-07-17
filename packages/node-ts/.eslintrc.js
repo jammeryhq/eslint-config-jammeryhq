dc64846ae9fcdd80584dafadc4ea746d30a7bf47
Travis Reynolds <travis@cirruscreative.co.uk>
2020-07-17 12:59:45 +0000

    Remove node & import

--- packages/node-ts/.eslintrc.js
+++ packages/node-ts/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended', 'plugin:node/recommended', 'plugin:import/typescript'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   rules: {
     indent: ['error', 2],
     semi: 'off',
